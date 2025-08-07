28ee7ade851904a3c05bb8db49b0b0bc64942000
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-07 15:11:49 +0200

    use different options for 22 and 24

--- .mocharc.js
+++ .mocharc.js
@@ -7,7 +7,9 @@ module.exports = {
   'timeout': '20000',
   'reporter': 'tap',
   'node-option':
-    (process.versions.node.split('.')[0] == 20 && process.versions.node.split('.')[1] >= 19) || process.versions.node.split('.')[0] >= 22 ? [
-      'no-experimental-require-module', 'no-experimental-strip-types', 'expose-gc'
-    ] : ['expose-gc']
+    [
+      ...((process.versions.node.split('.')[0] == 20 && process.versions.node.split('.')[1] >= 19) ? ['no-experimental-require-module'] : []),
+      ...(process.versions.node.split('.')[0] >= 22 ? ['no-experimental-strip-types'] : []),
+      'expose-gc'
+    ]
 };
